29159f701ad4c3d9bf505cba2f90c101f6500760
Ashutosh Kumar <ashutosh.kumar_cs18@gla.ac.in>
2020-03-31 20:51:35 +0530

    Problem 1 & 2 solved

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,26 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
-// executable class
+import problem1.mybst.MyBinarySearchTree;
+import java.util.Scanner;
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        // taking only 7 inputs for simple demonstration
+        int i = 7;
+        while(i>0){
+            tree.add(scanner.nextInt());
+            i--;
+        }
+        //input order is 4,2,1,3,6,5,7
+        /*tree looks like
+                    4
+                  /  \
+                2      6
+               / \    / \
+              1   3  5   7
+           */
+        tree.printLeftChild();
+        System.out.println(MyBinarySearchTree.noLeftCounter);
+    }
 }
